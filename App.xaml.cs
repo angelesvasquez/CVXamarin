b6dff735263713adc7622dca9358f23f20397044
mavp1519 <115326742+angelesvasquez@users.noreply.github.com>
2024-11-22 03:00:24 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace app_cv
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // La página principal puede ser MainPage.xaml o cualquier otra
+            MainPage = new MainPage();
+        }
+
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
